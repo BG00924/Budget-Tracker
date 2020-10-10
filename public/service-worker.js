37aca7efbdfe5dbe8cf1950f08553713712be6c3
BG00924 <BG00924@users.noreply.github.com>
2020-10-09 19:05:10 -0500

    typo

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,7 @@
 
 const FILES_TO_CACHE = [
-    "./index.html",
+    "/",
+    "/index.html",
     "./css/styles.css",
     "./js/idb.js",
     "./js/index.js",
@@ -49,7 +50,13 @@ self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url)
     e.respondWith(
         caches.match(e.request).then(function (request) {
-            return request || fetch(e.request)
+            if (request) { // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+              } else {       // if there are no cache, try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+              }
         })
     )
 })
